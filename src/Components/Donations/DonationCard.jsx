4cabc6d3f9ab56d0857957a92642ea08f38284e6
MohsenAlMahmud <mohsen.al.mahmud@gmail.com>
2023-09-27 05:07:20 +0600

    make some corrections

--- src/Components/Donations/DonationCard.jsx
+++ src/Components/Donations/DonationCard.jsx
@@ -8,7 +8,7 @@ const DonationCard = ({ donation }) => {
             <div className="">
                 <div className="card">
                     <figure><img src={picture} alt="" /></figure>
-                    <div style={{ backgroundColor: category_bg_color }} className="p-6">
+                    <div style={{ backgroundColor: category_bg_color }} className="p-6 rounded-md">
                         <div className="card-actions">
                             <button style={{ color: text_color, backgroundColor: button_bg_color }} className="rounded-md px-4 font-medium">{category}</button>
                         </div>
